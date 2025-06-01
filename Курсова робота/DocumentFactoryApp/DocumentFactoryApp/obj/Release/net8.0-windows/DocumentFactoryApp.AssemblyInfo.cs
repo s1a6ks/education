369c3f1d8a3f3aed7912a2f4b5adbf187334fb82
Horetskyi Maksym <s1a6ks@gmail.com>
2025-06-01 19:13:15 +0300

    s

--- Курсова робота/DocumentFactoryApp/DocumentFactoryApp/obj/Release/net8.0-windows/DocumentFactoryApp.AssemblyInfo.cs
+++ Курсова робота/DocumentFactoryApp/DocumentFactoryApp/obj/Release/net8.0-windows/DocumentFactoryApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocumentFactoryApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23979a5db5380d041b8a298a65cc64e96a2bd329")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocumentFactoryApp")]
